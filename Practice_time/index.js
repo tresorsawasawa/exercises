2a1494e88548a19d62c79eb21fbe997ce19181b7
Trésor Sawasawa <83042742+tresorsawasawa@users.noreply.github.com>
2021-08-22 23:56:20 +0200

    Add files via upload

---
+++ Practice_time/index.js
@@ -0,0 +1,33 @@
+// let name = "Tresor";
+// let greeting = "Hi there"
+
+// function string() {
+//     console.log(greetin +", "+ nqme)
+// }
+// string()
+
+// let myPoints = 3;
+
+// function add3Points() {
+//     myPoint += 3;
+// }
+// function remove1Points() {
+//     myPoint -= 1;
+// }
+// add3Points()
+// add3Points()
+// add3Points()
+// remove1Points()
+// remove1Points()
+
+// console.log(myPoints)
+
+// let errorParagraph = document.getElementById("error")
+// console.log(errorParagraph);
+
+// function purchase() {
+//     console.log("button clicked");
+//     errorParagraph.textContent = "Some thing went wrong, please try again";  
+// }
+
+ 
