6b2657a7d009a7f4523a6dedaa5a2b7966f83974
Trésor Sawasawa <83042742+tresorsawasawa@users.noreply.github.com>
2021-09-12 23:04:08 +0200

    Add files via upload

---
+++ Quiz_app/script.js
@@ -0,0 +1,27 @@
+const quiz = [
+    {
+        q:"Which month comes right before September ?",
+        Option: ['July','August','October','November'],
+        answer: o
+    },
+    {
+        q:"What color is a banana?",
+        Option: ['Red','Blue','Yellow','Orange'],
+        answer: 1
+    },
+    {
+        q:"9 - 3 * 4 + 5 = 2 ?",
+        Option: ['true','false'],
+        answer: 2
+    },
+    {
+        q:"What time of the day do we have breakfast?",
+        Option: ['In the morning','In the evening', 'In the afternoon'],
+        answer: 3
+    },
+    {
+        q:"What is 18 + 3 ?",
+        Option: ['83','38', '183', '21'],
+        answer: 3
+    }
+]  
