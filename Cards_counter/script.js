e8b762f58b6a0471ca30eea919f4e71776fc77b2
Trésor Sawasawa <83042742+tresorsawasawa@users.noreply.github.com>
2021-09-20 23:20:41 +0200

    Add files via upload

---
+++ Cards_counter/script.js
@@ -0,0 +1,30 @@
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } 
+  else {
+    return count + " Hold";
+  }
+  // Only change code above this line
+}
+
+console.log(cc(2)); console.log(cc(4)); console.log(cc(7)); console.log(cc("K")); console.log(cc("A"));
